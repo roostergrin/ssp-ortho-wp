07d468474573f3e7608f1b87e6f2f31792f93e3c
Sam Tweedy <stweedy.st@gmail.com>
2023-06-02 16:40:40 -0400

    even more testing

--- simko/images/svgs/colors/inline-orange.svg.php
+++ simko/images/svgs/colors/inline-orange.svg.php
@@ -1,1 +1,1 @@
-<svg xmlns="http://www.w3.org/2000/svg" width="100%" viewBox="0 0 100 60"><defs><style>.orange-a{fill:#e98c25;}</style></defs><rect class="blue-a" width="100" height="60"/></svg>
+<svg xmlns="http://www.w3.org/2000/svg" width="100%" viewBox="0 0 100 60"><defs><style>.orange-a{fill:#e98c25;}</style></defs><rect class="orange-a" width="100" height="60"/></svg>
