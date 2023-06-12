6e77553bb9b1a8e9fffecce7e1936bf322cbdf23
phamiter253 <34218345+phamiter253@users.noreply.github.com>
2023-06-12 12:23:21 -0700

    removing greatriver custom mouth guard

--- simko/partials/section/header.php
+++ simko/partials/section/header.php
@@ -15,10 +15,6 @@ $service_templates = [
 	'smile-gallery.php'
 ];
 
-if($location->ID !== 3393 || $location->ID !== 3396){
-	//unset( $services['free-custom-mouthguard'] );
-	echo "<script>console.log('Debug Objects: " . $location->ID . "' );</script>";
-}
 
 $template_array = explode('/', get_page_template());
 $is_landing_page = if_landing_page_get_lp_phone() ?? false;
