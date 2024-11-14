02ddb8ca6189b8c18b29a8ad5acf7a31e12182fb
phamiter253 <34218345+phamiter253@users.noreply.github.com>
2024-11-14 13:40:54 -0800

    commenting out shortcode

--- simko/partials/section/footer.php
+++ simko/partials/section/footer.php
@@ -106,5 +106,5 @@ if(!empty($location)) {
             <div class="disclaimer"><p><?= $brand->footer_trademark_text; ?></p></div>
 		</div>
 	</div>
-	<?php echo do_shortcode("[gtranslate]"); ?>
+	<!-- <?php echo do_shortcode("[gtranslate]"); ?> -->
 </section>
