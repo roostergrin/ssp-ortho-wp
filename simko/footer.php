cf986d8bde1cfce319e03878ebc68d1cbc6ee19b
phamiter253 <34218345+phamiter253@users.noreply.github.com>
2024-11-14 13:13:14 -0800

    fixing shortcode

--- simko/footer.php
+++ simko/footer.php
@@ -3,10 +3,11 @@
 	partial('section.footer');
 	wp_footer();
 	$brand = is_brand();
+	if( $brand->ID === 13032 || $brand->ID === 13590){
+		echo do_shortcode('[gtranslate]')
+	}	
 ?>
-<?php if( $brand->ID === 13032 || $brand->ID === 13590): ?>
-	echo do_shortcode('[gtranslate]');
-<?php endif; ?>	
+
 		</main>
 	</body>
 </html>
