b5368bc0e806c53cd194fc4da21e05cbb3c47556
phamiter253 <34218345+phamiter253@users.noreply.github.com>
2024-11-14 13:10:02 -0800

    testing translate widget

--- simko/footer.php
+++ simko/footer.php
@@ -2,7 +2,11 @@
 <?
 	partial('section.footer');
 	wp_footer();
+	$brand = is_brand();
 ?>
+<?php if( $brand->ID === 13032 || $brand->ID === 13590): ?>
+	echo do_shortcode('[gtranslate]');
+<?php endif; ?>	
 		</main>
 	</body>
 </html>
